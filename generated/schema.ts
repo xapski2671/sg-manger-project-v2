395c9e8120541873b00570f5baf2042386bdd14a
xapski <xapski2671@gmail.com>
2023-02-28 18:41:13 +0100

    cmt01

--- generated/schema.ts
+++ generated/schema.ts
@@ -11,7 +11,7 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
-export class ExampleEntity extends Entity {
+export class UserAdded extends Entity {
   constructor(id: string) {
     super();
     this.set("id", Value.fromString(id));
@@ -19,18 +19,18 @@ export class ExampleEntity extends Entity {
 
   save(): void {
     let id = this.get("id");
-    assert(id != null, "Cannot save ExampleEntity entity without an ID");
+    assert(id != null, "Cannot save UserAdded entity without an ID");
     if (id) {
       assert(
         id.kind == ValueKind.STRING,
-        `Entities of type ExampleEntity must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+        `Entities of type UserAdded must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
       );
-      store.set("ExampleEntity", id.toString(), this);
+      store.set("UserAdded", id.toString(), this);
     }
   }
 
-  static load(id: string): ExampleEntity | null {
-    return changetype<ExampleEntity | null>(store.get("ExampleEntity", id));
+  static load(id: string): UserAdded | null {
+    return changetype<UserAdded | null>(store.get("UserAdded", id));
   }
 
   get id(): string {
@@ -42,30 +42,439 @@ export class ExampleEntity extends Entity {
     this.set("id", Value.fromString(value));
   }
 
-  get count(): BigInt {
-    let value = this.get("count");
+  get address(): Bytes {
+    let value = this.get("address");
+    return value!.toBytes();
+  }
+
+  set address(value: Bytes) {
+    this.set("address", Value.fromBytes(value));
+  }
+
+  get username(): string {
+    let value = this.get("username");
+    return value!.toString();
+  }
+
+  set username(value: string) {
+    this.set("username", Value.fromString(value));
+  }
+
+  get twitter(): string | null {
+    let value = this.get("twitter");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set twitter(value: string | null) {
+    if (!value) {
+      this.unset("twitter");
+    } else {
+      this.set("twitter", Value.fromString(<string>value));
+    }
+  }
+
+  get email(): string | null {
+    let value = this.get("email");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set email(value: string | null) {
+    if (!value) {
+      this.unset("email");
+    } else {
+      this.set("email", Value.fromString(<string>value));
+    }
+  }
+
+  get bio(): string | null {
+    let value = this.get("bio");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set bio(value: string | null) {
+    if (!value) {
+      this.unset("bio");
+    } else {
+      this.set("bio", Value.fromString(<string>value));
+    }
+  }
+
+  get createdCount(): BigInt | null {
+    let value = this.get("createdCount");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toBigInt();
+    }
+  }
+
+  set createdCount(value: BigInt | null) {
+    if (!value) {
+      this.unset("createdCount");
+    } else {
+      this.set("createdCount", Value.fromBigInt(<BigInt>value));
+    }
+  }
+
+  get backedCount(): BigInt | null {
+    let value = this.get("backedCount");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toBigInt();
+    }
+  }
+
+  set backedCount(value: BigInt | null) {
+    if (!value) {
+      this.unset("backedCount");
+    } else {
+      this.set("backedCount", Value.fromBigInt(<BigInt>value));
+    }
+  }
+
+  get createdAt(): BigInt {
+    let value = this.get("createdAt");
     return value!.toBigInt();
   }
 
-  set count(value: BigInt) {
-    this.set("count", Value.fromBigInt(value));
+  set createdAt(value: BigInt) {
+    this.set("createdAt", Value.fromBigInt(value));
+  }
+}
+
+export class CampaignAdded extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
   }
 
-  get _campaignAddress(): Bytes {
-    let value = this.get("_campaignAddress");
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save CampaignAdded entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type CampaignAdded must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("CampaignAdded", id.toString(), this);
+    }
+  }
+
+  static load(id: string): CampaignAdded | null {
+    return changetype<CampaignAdded | null>(store.get("CampaignAdded", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get campaignAddress(): Bytes {
+    let value = this.get("campaignAddress");
     return value!.toBytes();
   }
 
-  set _campaignAddress(value: Bytes) {
-    this.set("_campaignAddress", Value.fromBytes(value));
+  set campaignAddress(value: Bytes) {
+    this.set("campaignAddress", Value.fromBytes(value));
   }
 
-  get _creator(): Bytes {
-    let value = this.get("_creator");
+  get creator(): Bytes {
+    let value = this.get("creator");
     return value!.toBytes();
   }
 
-  set _creator(value: Bytes) {
-    this.set("_creator", Value.fromBytes(value));
+  set creator(value: Bytes) {
+    this.set("creator", Value.fromBytes(value));
+  }
+
+  get title(): string {
+    let value = this.get("title");
+    return value!.toString();
+  }
+
+  set title(value: string) {
+    this.set("title", Value.fromString(value));
+  }
+
+  get description(): string {
+    let value = this.get("description");
+    return value!.toString();
+  }
+
+  set description(value: string) {
+    this.set("description", Value.fromString(value));
+  }
+
+  get category(): string {
+    let value = this.get("category");
+    return value!.toString();
+  }
+
+  set category(value: string) {
+    this.set("category", Value.fromString(value));
+  }
+
+  get tags(): Array<string> | null {
+    let value = this.get("tags");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toStringArray();
+    }
+  }
+
+  set tags(value: Array<string> | null) {
+    if (!value) {
+      this.unset("tags");
+    } else {
+      this.set("tags", Value.fromStringArray(<Array<string>>value));
+    }
+  }
+
+  get goalAmount(): BigInt {
+    let value = this.get("goalAmount");
+    return value!.toBigInt();
+  }
+
+  set goalAmount(value: BigInt) {
+    this.set("goalAmount", Value.fromBigInt(value));
+  }
+
+  get duration(): BigInt {
+    let value = this.get("duration");
+    return value!.toBigInt();
+  }
+
+  set duration(value: BigInt) {
+    this.set("duration", Value.fromBigInt(value));
+  }
+
+  get campaignURI(): string {
+    let value = this.get("campaignURI");
+    return value!.toString();
+  }
+
+  set campaignURI(value: string) {
+    this.set("campaignURI", Value.fromString(value));
+  }
+
+  get funders(): Array<Bytes> | null {
+    let value = this.get("funders");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toBytesArray();
+    }
+  }
+
+  set funders(value: Array<Bytes> | null) {
+    if (!value) {
+      this.unset("funders");
+    } else {
+      this.set("funders", Value.fromBytesArray(<Array<Bytes>>value));
+    }
+  }
+
+  get funderCount(): BigInt {
+    let value = this.get("funderCount");
+    return value!.toBigInt();
+  }
+
+  set funderCount(value: BigInt) {
+    this.set("funderCount", Value.fromBigInt(value));
+  }
+
+  get createdAt(): BigInt {
+    let value = this.get("createdAt");
+    return value!.toBigInt();
+  }
+
+  set createdAt(value: BigInt) {
+    this.set("createdAt", Value.fromBigInt(value));
+  }
+}
+
+export class CampaignFunded extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save CampaignFunded entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type CampaignFunded must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("CampaignFunded", id.toString(), this);
+    }
+  }
+
+  static load(id: string): CampaignFunded | null {
+    return changetype<CampaignFunded | null>(store.get("CampaignFunded", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get funder(): Bytes {
+    let value = this.get("funder");
+    return value!.toBytes();
+  }
+
+  set funder(value: Bytes) {
+    this.set("funder", Value.fromBytes(value));
+  }
+
+  get campaignAddress(): Bytes {
+    let value = this.get("campaignAddress");
+    return value!.toBytes();
+  }
+
+  set campaignAddress(value: Bytes) {
+    this.set("campaignAddress", Value.fromBytes(value));
+  }
+
+  get createdAt(): BigInt {
+    let value = this.get("createdAt");
+    return value!.toBigInt();
+  }
+
+  set createdAt(value: BigInt) {
+    this.set("createdAt", Value.fromBigInt(value));
+  }
+}
+
+export class CampaignShrunk extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save CampaignShrunk entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type CampaignShrunk must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("CampaignShrunk", id.toString(), this);
+    }
+  }
+
+  static load(id: string): CampaignShrunk | null {
+    return changetype<CampaignShrunk | null>(store.get("CampaignShrunk", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get withdrawer(): Bytes {
+    let value = this.get("withdrawer");
+    return value!.toBytes();
+  }
+
+  set withdrawer(value: Bytes) {
+    this.set("withdrawer", Value.fromBytes(value));
+  }
+
+  get campaignAddress(): Bytes {
+    let value = this.get("campaignAddress");
+    return value!.toBytes();
+  }
+
+  set campaignAddress(value: Bytes) {
+    this.set("campaignAddress", Value.fromBytes(value));
+  }
+
+  get createdAt(): BigInt {
+    let value = this.get("createdAt");
+    return value!.toBigInt();
+  }
+
+  set createdAt(value: BigInt) {
+    this.set("createdAt", Value.fromBigInt(value));
+  }
+}
+
+export class CampaignRemoved extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save CampaignRemoved entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type CampaignRemoved must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("CampaignRemoved", id.toString(), this);
+    }
+  }
+
+  static load(id: string): CampaignRemoved | null {
+    return changetype<CampaignRemoved | null>(store.get("CampaignRemoved", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get campaignAddress(): Bytes {
+    let value = this.get("campaignAddress");
+    return value!.toBytes();
+  }
+
+  set campaignAddress(value: Bytes) {
+    this.set("campaignAddress", Value.fromBytes(value));
+  }
+
+  get createdAt(): BigInt {
+    let value = this.get("createdAt");
+    return value!.toBigInt();
+  }
+
+  set createdAt(value: BigInt) {
+    this.set("createdAt", Value.fromBigInt(value));
   }
 }
--- src/crowd-funder.ts
+++ src/crowd-funder.ts
@@ -1,61 +1,106 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, store } from "@graphprotocol/graph-ts"
 import {
   CrowdFunder,
-  CampaignAdded,
-  CampaignFunded,
-  CampaignRemoved,
-  CampaignShrunk,
-  UserAdded
+  CampaignAdded as CampaignAddedEvent,
+  CampaignFunded as CampaignFundedEvent,
+  CampaignRemoved as CampaignRemovedEvent,
+  CampaignShrunk as CampaignShrunkEvent,
+  UserAdded as UserAddedEvent
 } from "../generated/CrowdFunder/CrowdFunder"
-import { ExampleEntity } from "../generated/schema"
+import { UserAdded, CampaignAdded, CampaignFunded, CampaignRemoved, CampaignShrunk } from "../generated/schema"
 
-export function handleCampaignAdded(event: CampaignAdded): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleCampaignAdded(event: CampaignAddedEvent): void {
+  let campaignAdded = CampaignAdded.load(event.params._campaignAddress.toHexString())
+  let userAdded = UserAdded.load(event.params._creator.toHexString())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if(!campaignAdded){
+    campaignAdded = new CampaignAdded(event.params._campaignAddress.toHexString())
+    campaignAdded.funderCount = BigInt.fromString("0")
+    campaignAdded.funders = []
+  }
+  if(!userAdded){
+    userAdded = new UserAdded(event.params._creator.toHexString())
+    userAdded.createdCount = BigInt.fromString("0")
+    userAdded.backedCount = BigInt.fromString("0")
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  campaignAdded.campaignAddress = event.params._campaignAddress
+  campaignAdded.creator = event.params._creator
+  campaignAdded.title = event.params._title
+  campaignAdded.description = event.params._description
+  campaignAdded.category = event.params._category
+  campaignAdded.tags = event.params._tags
+  campaignAdded.goalAmount = event.params._goalAmount
+  campaignAdded.duration = event.params._duration
+  campaignAdded.createdAt = event.block.timestamp
 
-  // Entity fields can be set based on event parameters
-  entity._campaignAddress = event.params._campaignAddress
-  entity._creator = event.params._creator
+  userAdded.createdCount = userAdded.createdCount!.plus(BigInt.fromString("1"))
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.campaignCounter(...)
-  // - contract.getCampaign(...)
+  campaignAdded.save()
+  userAdded.save()
 }
 
-export function handleCampaignFunded(event: CampaignFunded): void {}
+export function handleCampaignFunded(event: CampaignFundedEvent): void {
+  let campaignAdded = CampaignAdded.load(event.params._campaignAddress.toHexString())
+  let userAdded = UserAdded.load(event.params._funder.toHexString())
 
-export function handleCampaignRemoved(event: CampaignRemoved): void {}
+  if(!userAdded){
+    userAdded = new UserAdded(event.params._funder.toHexString())
+    userAdded.createdCount = BigInt.fromString("0")
+    userAdded.backedCount = BigInt.fromString("0")
+  }
 
-export function handleCampaignShrunk(event: CampaignShrunk): void {}
+  campaignAdded!.funders!.push(event.params._funder)
+  campaignAdded!.funderCount.plus(BigInt.fromString("1"))
 
-export function handleUserAdded(event: UserAdded): void {}
+  userAdded.backedCount = userAdded.backedCount!.plus(BigInt.fromString("1"))
+
+  campaignAdded!.save()
+  userAdded.save()
+}
+
+export function handleCampaignRemoved(event: CampaignRemovedEvent): void {
+  let id = event.params._campaignAddress.toHexString()
+  store.remove("CampaignAdded", id)
+}
+
+export function handleCampaignShrunk(event: CampaignShrunkEvent): void {
+  let campaignAdded = CampaignAdded.load(event.params._campaignAddress.toHexString())
+  let userAdded = UserAdded.load(event.params._withdrawer.toHexString())
+
+  if(!userAdded){
+    userAdded = new UserAdded(event.params._withdrawer.toHexString())
+    userAdded.createdCount = BigInt.fromString("0")
+    userAdded.backedCount = BigInt.fromString("0")
+  }
+
+  if(campaignAdded!.funders!.includes(event.params._withdrawer)){
+    const index = campaignAdded!.funders!.indexOf(event.params._withdrawer)
+    campaignAdded!.funders!.splice(index, 1)
+  }
+  campaignAdded!.funderCount.minus(BigInt.fromString("1"))
+
+  userAdded.backedCount = userAdded.backedCount!.minus(BigInt.fromString("1"))
+  
+  campaignAdded!.save()
+  userAdded.save()
+}
+
+export function handleUserAdded(event: UserAddedEvent): void {
+  let userAdded = UserAdded.load(event.params._address.toHexString())
+
+  if(!userAdded){
+    userAdded = new UserAdded(event.params._address.toHexString())
+    userAdded.createdCount = BigInt.fromString("0")
+    userAdded.backedCount = BigInt.fromString("0")
+  }
+
+  userAdded.address = event.params._address
+  userAdded.username = event.params._username
+  userAdded.twitter = event.params._twitter
+  userAdded.email = event.params._email
+  userAdded.bio = event.params._bio
+  userAdded.createdAt = event.block.timestamp
+
+  userAdded.save()
+}
